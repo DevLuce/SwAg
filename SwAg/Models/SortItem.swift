bdfea5d7fbe9c2aabf77861b09fb313ffed40a11
Wonhyuk Choi <whchoi000@gmail.com>
2022-05-04 09:28:16 +0900

    Edit sort datas

--- SwAg/Models/SortItem.swift
+++ SwAg/Models/SortItem.swift
@@ -17,8 +17,6 @@ struct SortItem: Hashable, Decodable, Identifiable {
             switch color {
             case "blue":
                 return .blue
-            case "gray":
-                return .gray
             case "purple":
                 return .purple
             case "indigo":
@@ -29,6 +27,14 @@ struct SortItem: Hashable, Decodable, Identifiable {
                 return .green
             case "red":
                 return .red
+            case "teal":
+                return .teal
+            case "act":
+                return Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1))
+            case "deact":
+                return Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1))
+            case "sel":
+                return Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
             default:
                 return .yellow
             }
